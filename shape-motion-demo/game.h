542d19d1b72494bcfaa6954c632477b1dbd9d6e2
Jose Perez <jperez50@miners.utep.edu>
2016-11-30 20:03:16 -0700

    Deleted our code

--- shape-motion-demo/game.h
+++
@@ -1,6 +0,0 @@
-#ifndef state_machine
-#define state_machine
-
-void state_update();
-
-#endif
--- shape-motion-demo/project_main.c
+++
@@ -1,7 +0,0 @@
-#include <msp430.h>
-#include "game.h"
-#include "switches.h"
-#include <lcdutils.h>
-#include <shape.h>
-
-
